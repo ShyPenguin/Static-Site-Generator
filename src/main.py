895dc056b952d0c101fded9ab014043b7962fedf
ShyPenguin <domatojawad@gmail.com>
2026-04-21 21:38:13 +0800

    build with new path

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ def main():
     base_path = "/"
     if len(sys.argv) > 1:
         base_path = sys.argv[1]
+        print(base_path)
     generated_dir = "docs" 
     move_static(generated_dir, "static")
     generate_pages_recursive("content", "template.html", generated_dir, base_path)
